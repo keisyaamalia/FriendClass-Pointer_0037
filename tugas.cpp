9a9b65345f51c9988968387659eab69c205f0377
keisyaamalia <kkaysaamalia@gmail.com>
2026-04-27 15:59:47 +0700

    inisialisasi program c++

---
+++ tugas.cpp
@@ -0,0 +1,9 @@
+// Buatlah sebuah program yang berisi 2 class dimana terdapat class Layang-Layang, Belah Ketupat.
+// Di setiap class nya berisikan luas dan keliling serta input dan output.
+// Setiap variable memiliki akses private, BangunDatar Belah Ketupat merupakan friend function keliling dari Layang-Layang, dan 
+// semua class hanya dapat diakses menggunakan object.
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
