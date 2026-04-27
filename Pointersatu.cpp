28f01c75f3eca9fad985ea1a65358f49a22bb78b
keisyaamalia <kkaysaamalia@gmail.com>
2026-04-27 12:41:39 +0700

    menjalankan program

--- Pointersatu.cpp
+++ Pointersatu.cpp
@@ -8,3 +8,16 @@ class mahasiswa
         cout << "No Induk = "<<nim<<endl;
     }
 };
+int main (){
+    mahasiswa Mhs{1}; //Object mhs
+    Mhs.showNim(); //Member Access Operator
+
+    mahasiswa &refMhs = Mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access Operator
+    Mhs.showNim();
+
+    mahasiswa *pMhs = &Mhs; //Pointer Dereferene pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
