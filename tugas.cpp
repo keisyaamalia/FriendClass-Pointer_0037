498da79353b4920fc2b0e43d119db060b36d1edb
keisyaamalia <kkaysaamalia@gmail.com>
2026-04-27 16:11:48 +0700

    akses modifier dan input class layang layang

--- tugas.cpp
+++ tugas.cpp
@@ -11,6 +11,34 @@ using namespace std;
 class BelahKetupat;
 class LayangLayang
 {
+private:
+    double d1, d2;
+    double sisi1, sisi2;
+
+public:
+    void inputData()
+        {
+            cout << "=== Input Layang-Layang ===" << endl;
+            cout << "Masukkan diagonal 1 (d1): ";
+            cin >> d1;
+            cout << "Masukkan diagonal 2 (d2): ";
+            cin >> d2;
+            cout << "Masukkan sisi 1        : ";
+            cin >> sisi1;
+            cout << "Masukkan sisi 2        : ";
+            cin >> sisi2;
+        }
+
+
+    double hitungLuas()
+    {
+        return 0.5 * d1 * d2;
+    }
+    
+        double hitungKeliling()
+        {
+            return 2 * (sisi1 + sisi2);
+        }
 
 };
 
